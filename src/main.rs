f47ed651fc4cd014b5d9209fb8c2eaaca7d65abb
Yannick Thimmesch <yannick.thimmesch@gmail.com>
2026-01-30 00:24:43 +0100

    Functional servo with analog input

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,11 @@ use panic_halt as _;
 use arduino_hal::default_serial;
 use ufmt::uwriteln;
 
+fn scale_joystick_degrees(i: u16) -> u8 {
+    ((i as u32 * 100000 / 1024 * 180) / 100000) as u8
+}
+
+
 #[arduino_hal::entry]
 fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
@@ -17,86 +22,45 @@ fn main() -> ! {
     let mut serial = default_serial!(dp, pins, 57600);
 
     let _ = uwriteln!(&mut serial, "Initializing Arduino Mega...");
-    // // pin 11 PWM setup TC1 channel A
-    // let tc1 = dp.TC1;
-    // tc1.tccr1a().reset();
-    // tc1.tccr1b().reset();
-    // tc1.tccr1c().reset();
-    // // Compare mode set-up in register TCCR1A
-    // // - bits 7:6 - COM1A1:0 = Output compare mode for OC1A (for channel A)
-    // // - bits 5:4 - COM1B1:0 = Output compare mode for OC1B (for channel B)
-    // // - bits 3:2 - COM1C1:0 = Output compare mode for OC1C (for channel C)
-    // // WGM 010 => FastPWM with ICRn to define TOP and OCRNX to define compare output.
-    // tc1.tccr1a().write(|w| unsafe {w.com1a().bits(0b10).wgm1().bits(0b10)}); // non-inverting mode Compare output mode for fast-pwm
-    // tc1.tccr1b().write(|w| unsafe {w.wgm1().bits(0b11).cs1().prescale_8()});
     
-    // // Setting TOP to IRC1 to achieve 50 Hz cycle.
-    // tc1.icr1().write(|w| w.set(39999u16));
-    
-    // // Setting output compare on channel C to define the PWM duty cycle.
-    // tc1.ocr1a().write(|w| w.set(0u16));
-    
-    // // Toggle pin 11 to output the OC3A output.
-    // pins.d11.into_output();
-    
-    // // pin 3 PWM setup with TC3 channel B
-    let tc3 = dp.TC3;
-    tc3.tccr3a().reset();
-    tc3.tccr3b().reset();
-    tc3.tccr3c().reset();
-    tc3.tccr3a().write(|w| unsafe{ w.com3a().bits(0b10).wgm3().bits(0b10)});
-    tc3.tccr3b().write(|w| unsafe{ w.wgm3().bits(0b11).cs3().prescale_64()});
-    
-    // Setting TOP to define PWM cycle.
-    tc3.icr3().write(|w| w.set(4999u16));
-    
-    // Setting duty cycle on channel C.
-    tc3.ocr3a().write(|w| w.set(0u16));
-    
-    pins.d3.into_output();
-    
-    let ocr3c_val = tc3.ocr3c().read().bits();
-    
-    // let _ = ufmt::uwrite!(&mut serial, "\n\rOCR1A = {}, READ = {}", i, ocr3c_val);
-    // let servo = Servo::into_servo();
-    // let _ = debug_dump(&mut serial, &tc1);
-    // SG90 0degrees = 86
-    // SG90 180 degrees = 660
     let tc1 = dp.TC1;
-    let servo = ServoPin::new(&tc1, pins.d11);
+    let mut servo = ServoPin::new(&tc1, pins.d11);
+    // servo.set_mode(ServoMode::Precise);
 
+    let mut adc = arduino_hal::Adc::new(dp.ADC, Default::default());
+    let a_x = pins.a0.into_analog_input(&mut adc);
+    let a_y = pins.a1.into_analog_input(&mut adc);
+
+    // let channels: [arduino_hal::adc::Channel; 2] = [
+    //     v_x.into_channel(),
+    //     v_y.into_channel(),
+    // ];
 
     let _ = uwriteln!(&mut serial, "TCCR1A {}", tc1.tccr1a().read().bits());
-    let _ = uwriteln!(&mut serial, "TCCR3A {}", tc3.tccr3a().read().bits());
     let _ = uwriteln!(&mut serial, "TCCR1B {}", tc1.tccr1b().read().bits());
-    let _ = uwriteln!(&mut serial, "TCCR3B {}", tc3.tccr3b().read().bits());
-
     
+    // Joystick min = 0
+    // Joystick max = 1023
+   
     loop {
         // for i in 125..625u16 {
-        let val = servo.set_rotation(0);
-        let _ = uwriteln!(&mut serial, "\n\rValue: {}", val);
-        let _ = uwriteln!(&mut serial, "\n\rOCR1A: {}", tc1.ocr1a().read().bits());
-        arduino_hal::delay_ms(1000);
-
-        let val = servo.set_rotation(90);
-        let _ = uwriteln!(&mut serial, "\n\rValue: {}", val);
-        let _ = uwriteln!(&mut serial, "\n\rOCR1A: {}", tc1.ocr1a().read().bits());
-        arduino_hal::delay_ms(1000);
+        // for i in [0, 90, 0, 90, 0, 180, 179, 178, 160, 120] {
+        //     let val = servo.set_rotation(i);
+        //     // let _ = uwriteln!(&mut serial, "\n\rValue: {}", val);
+        //     // let _ = uwriteln!(&mut serial, "\n\rOCR1A: {}", tc1.ocr1a().read().bits());
+        //     arduino_hal::delay_ms(500);
+        // }
+        // for ch in channels.iter() {
+        //     let adc.read_blocking(ch);
+        // }
+        let v_x = a_x.analog_read(&mut adc);
+        let v_y = a_y.analog_read(&mut adc);
         
-        // }
-        // for i in (125..625u16).rev() {
-            let val = servo.set_rotation(180);
-            let _ = uwriteln!(&mut serial, "\n\rValue: {}", val);
-            let _ = uwriteln!(&mut serial, "\n\rOCR1A: {}", tc1.ocr1a().read().bits());
-        // let _ = uwriteln!(&mut serial, "\n\rOCR1A: {}", tc1.ocr1a().read().bits());
-        arduino_hal::delay_ms(1000);
-        // }
-        // // arduino_hal::delay_ms(2000);
-        // for i in 0..499u16.reverse_bits() {
-        //     tc1.ocr1a().write(|w| w.set(i));
-        //     arduino_hal::delay_ms(25);
-        //     let _ = ufmt::uwrite!(&mut serial, "\n\rOCR1A = {}", i);
-        // }
+        let rotation = scale_joystick_degrees(v_x);
+        servo.set_rotation(rotation);
+
+        let _ = uwriteln!(&mut serial, "X: {}, Y: {}, rotation: {}", v_x, v_y, rotation);
+
+        // arduino_hal::delay_ms(100);
     }
 }
