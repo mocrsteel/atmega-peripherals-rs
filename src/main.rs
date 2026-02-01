341622fd815f769bc072bb336b0cb159a8b4707e
Yannick Thimmesch <yannick.thimmesch@gmail.com>
2026-02-01 22:45:00 +0100

    fix: workflow errors

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 #![no_std]
 #![no_main]
 
+use arduino_hal::default_serial;
 use arduino_hal::prelude::*;
-use arduino_hal::{default_serial};
 use panic_halt as _;
 use ufmt::uwriteln;
 
@@ -16,10 +16,10 @@ fn main() -> ! {
 
     let tc = dp.TC3;
     let servo = ServoPin::new(&tc, pins.d5);
-    
+
     // Joystick min = 0
     // Joystick max = 1023
-   
+
     loop {
         for deg in [0, 45, 90, 135, 180, 135, 90, 45] {
             servo.set_rotation(deg as u8);
