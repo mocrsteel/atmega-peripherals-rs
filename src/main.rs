e4f6564f1407c32cb424f338cd690ea20312aa03
Yannick Thimmesch <yannick.thimmesch@gmail.com>
2026-02-22 19:10:17 +0100

    max7219 wip

--- src/main.rs
+++ src/main.rs
@@ -9,24 +9,23 @@ use panic_halt as _;
 use ufmt::uwriteln;
 
 use avr_servo::{ServoPin, ServoPinOps};
+use max7219::*;
 
 #[arduino_hal::entry]
 fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
-    let mut serial = default_serial!(dp, pins, 57600);
 
-    let tc = dp.TC3;
-    let servo = ServoPin::new(&tc, pins.d5);
+    let mut data_pin = pins.d3.into_output().downgrade();
+    let mut cs_pin = pins.d4.into_output().downgrade();
+    let mut clk_pin = pins.d5.into_output().downgrade();
+    // let tc = dp.TC3;
+    // let servo = ServoPin::new(&tc, pins.d5);
 
     // Joystick min = 0
     // Joystick max = 1023
+    send_data(&mut data_pin, &mut clk_pin, &mut cs_pin, 0b00001111, Address::DIGIT0);
 
     loop {
-        for deg in [0, 45, 90, 135, 180, 135, 90, 45] {
-            servo.set_rotation(deg as u8);
-            uwriteln!(&mut serial, "Rotating to {} degrees", deg).unwrap_infallible();
-            arduino_hal::delay_ms(500);
-        }
     }
 }
--- src/max7219.rs
+++ src/max7219.rs
@@ -1,49 +1,52 @@
 //! Starting point for a driver for the MAX7219 8x8 LED matrix.
 #![allow(dead_code)]
 
+use arduino_hal::hal::port::{mode, Dynamic};
 use arduino_hal::port::Pin;
-use arduino_hal::hal::port::{Dynamic, mode};
 
-// -- Register addresses per digit (rows?)
-const DIG_0: u8 = 0x0;
-const DIG_1: u8 = 0x1;
-const DIG_2: u8 = 0x2;
-const DIG_3: u8 = 0x3;
-const DIG_4: u8 = 0x4;
-const DIG_5: u8 = 0x5;
-const DIG_6: u8 = 0x6;
-const DIG_7: u8 = 0x7;
-const NO_OP: u8 = 0x0;
-const MODE_DECODE: u8 = 0x9;
-const INTENSITY: u8 = 0xA;
-const SCAN_LIMIT: u8 = 0xB;
-const SHUTDOWN: u8 = 0xC;
-const DISPLAY_TEST: u8 = 0xF;
-
-// Intensity modes
-const INTENSITY_MIN: u8 = 0x0;
-const INTENSITY_MAX: u8 = 0xF;
-
-pub enum ADDRESS {
-    DIG_0,
-    DIG_1,
-    DIG_2,
-    DIG_3,
-    DIG_4,
-    DIG_5,
-    DIG_6,
-    DIG_7,
-    NO_OP,
-    MODE_DECODE,
-    INTENSITY,
-    SCAN_LIMIT,
-    SHUTDOWN,
-    DISPLAY_TEST,
+/// Possible register addresses for the MAX7219.
+pub struct Address;
+impl Address {
+    pub const DIGIT0: u8 = 0x0;
+    pub const DIGIT1: u8 = 0x1;
+    pub const DIGIT2: u8 = 0x2;
+    pub const DIGIT3: u8 = 0x3;
+    pub const DIGIT4: u8 = 0x4;
+    pub const DIGIT5: u8 = 0x5;
+    pub const DIGIT6: u8 = 0x6;
+    pub const DIGIT7: u8 = 0x7;
+    pub const NO_OP: u8 = 0x8;
+    pub const DECODE_MODE: u8 = 0x9;
+    pub const INTENSITY: u8 = 0xa;
+    pub const SCAN_LIMIT: u8 = 0xb;
+    pub const SHUTDOWN: u8 = 0xc;
+    pub const DISPLAY_TEST: u8 = 0xf;
 }
+
+pub struct Intensity;
+impl Intensity {
+    pub const MIN: u8 = 0x0;
+    pub const MAX: u8 = 0xf;
+
+    /// Get a validated intensity value for the MAX7219.
+    /// Ensures that the value is between MIN and MAX (0x0 and 0xf).
+    pub fn val(intensity: u8) -> u8 {
+        if intensity < Self::MIN {
+            Self::MIN
+        } else if intensity > Self::MAX {
+            Self::MAX
+        } else {
+            intensity
+        }
+    }
+}
+
 // Connectivity:
-// * MOSI to DIN
-// * I/O to LOAD(CS)
-// * SCK to CLK
+// * clk pin to CLK
+// * cs pin to CS
+// * data pin to DIN
+// * 5V to VCC
+// * GND to GND
 
 // SPI data
 // * CLK period min = 100 ns
@@ -56,10 +59,32 @@ pub enum ADDRESS {
 // MAx7219 needs to receive the MSB first.
 
 /// Manual bit bang code.
-pub fn send_data<'a>(pin: &'a mut Pin<mode::Output,>, clk: &'a mut Pin<mode::Output,>, data: u8, address: u8) {
+/// MSB first
+pub fn send_data<'a>(
+    data_pin: &'a mut Pin<mode::Output>,
+    clk_pin: &'a mut Pin<mode::Output>,
+    cs_pin: &'a mut Pin<mode::Output>,
+    data: u8,
+    address: u8,
+) {
     // Combine the address to the first 8 bits and then append the data in the last 8 bits.
-    let serialized = (address as u16) << 8 | (data as u16); 
+    let mut serialized = (address as u16) << 8 | (data as u16);
+    // prepare to receive data.
+    cs_pin.set_low();
+    clk_pin.set_low();
 
-    clk.set_low();
-
+    // Serially shift the 16 data bits into shift register (address + data).
+    for _ in 0..16 {
+        match (serialized >> 15) & 1 == 1 {
+            false => data_pin.set_low(),
+            true => data_pin.set_high(),
+        };
+        serialized <<= 1;
+        clk_pin.set_high();
+        arduino_hal::delay_ns(50);
+        clk_pin.set_low();
+        arduino_hal::delay_ns(50);
+    }
+    // pulse to load to register address.
+    cs_pin.set_high();
 }
