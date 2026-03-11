f979591415d2a8f1159e54374ffd7be0e3cb1f43
Yannick Thimmesch <yannick.thimmesch@gmail.com>
2026-03-11 22:29:41 +0100

    max7219 working bit bang

--- src/main.rs
+++ src/main.rs
@@ -3,32 +3,61 @@
 
 mod max7219;
 
-use arduino_hal::Spi;
 use arduino_hal::default_serial;
 use arduino_hal::prelude::*;
 use panic_halt as _;
 use ufmt::uwriteln;
 
-use avr_servo::{ServoPin, ServoPinOps};
+// use avr_servo::{ServoPin, ServoPinOps};
 use max7219::*;
 
 #[arduino_hal::entry]
 fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
-
+    let mut serial = default_serial!(dp, pins, 57600);
     // let mut spi = Spi::new(dp.SPI, pins.d52.into_output(), pins.d51.into_output(), pins.d50.into_pull_up_input(), pins.d53.into_output(), arduino_hal::spi::Settings::default());
 
     let mut data_pin = pins.d3.into_output().downgrade();
     let mut cs_pin = pins.d4.into_output().downgrade();
     let mut clk_pin = pins.d5.into_output().downgrade();
-    // let tc = dp.TC3;
-    // let servo = ServoPin::new(&tc, pins.d5);
 
-    // Joystick min = 0
-    // Joystick max = 1023
-    send_data(&mut data_pin, &mut clk_pin, &mut cs_pin, 0b00001111, Address::DIGIT0);
+    let address: u8 = 0xF;
+    let data: u8 = 0x1;
 
+    send_data(&mut data_pin, &mut clk_pin, &mut cs_pin, 0x1u8, Address::DISPLAY_TEST);
+    arduino_hal::delay_ms(1000);
+    send_data(&mut data_pin, &mut clk_pin, &mut cs_pin, 0x0u8, Address::DISPLAY_TEST);
+    send_data(&mut data_pin, &mut clk_pin, &mut cs_pin, 0x0u8, Address::DECODE_MODE);
+    send_data(&mut data_pin, &mut clk_pin, &mut cs_pin, 0x7u8, Address::SCAN_LIMIT);
+    let flag_down: [u8; 8] = [
+        0b00000000,
+        0b00000001,
+        0b00000011,
+        0b00000111,
+        0b00001111,
+        0b00011111,
+        0b00111111,
+        0b01111111,
+    ];
+    let flag_up: [u8; 8] = [
+        0b11111110,
+        0b11111100,
+        0b11111000,
+        0b11110000,
+        0b11100000,
+        0b11000000,
+        0b10000000,
+        0b00000000,
+    ];
+    
+    send_data(&mut data_pin, &mut clk_pin, &mut cs_pin, 0x1u8, Address::INTENSITY);
+    
     loop {
+        set_matrix(&mut data_pin, &mut clk_pin, &mut cs_pin, flag_up);
+        arduino_hal::delay_ms(500);
+        set_matrix(&mut data_pin, &mut clk_pin, &mut cs_pin, flag_down);
+        arduino_hal::delay_ms(500);
+
     }
 }
--- src/max7219.rs
+++ src/max7219.rs
@@ -1,21 +1,21 @@
 //! Starting point for a driver for the MAX7219 8x8 LED matrix.
 #![allow(dead_code)]
 
-use arduino_hal::hal::port::{mode, Dynamic};
+use arduino_hal::hal::port::{mode::Output, Dynamic};
 use arduino_hal::port::Pin;
 
 /// Possible register addresses for the MAX7219.
 pub struct Address;
 impl Address {
-    pub const DIGIT0: u8 = 0x0;
-    pub const DIGIT1: u8 = 0x1;
-    pub const DIGIT2: u8 = 0x2;
-    pub const DIGIT3: u8 = 0x3;
-    pub const DIGIT4: u8 = 0x4;
-    pub const DIGIT5: u8 = 0x5;
-    pub const DIGIT6: u8 = 0x6;
-    pub const DIGIT7: u8 = 0x7;
-    pub const NO_OP: u8 = 0x8;
+    pub const DIGIT0: u8 = 0x1;
+    pub const DIGIT1: u8 = 0x2;
+    pub const DIGIT2: u8 = 0x3;
+    pub const DIGIT3: u8 = 0x4;
+    pub const DIGIT4: u8 = 0x5;
+    pub const DIGIT5: u8 = 0x6;
+    pub const DIGIT6: u8 = 0x7;
+    pub const DIGIT7: u8 = 0x8;
+    pub const NO_OP: u8 = 0x0;
     pub const DECODE_MODE: u8 = 0x9;
     pub const INTENSITY: u8 = 0xa;
     pub const SCAN_LIMIT: u8 = 0xb;
@@ -23,6 +23,8 @@ impl Address {
     pub const DISPLAY_TEST: u8 = 0xf;
 }
 
+/// Intensity for MAX7219. Can be set to MAX `0xf` or MIN `0x0`.
+/// Can be set to intermediate values using [Intensity::val()]
 pub struct Intensity;
 impl Intensity {
     pub const MIN: u8 = 0x0;
@@ -58,20 +60,22 @@ impl Intensity {
 // D7 - D0: MSB to LSB of data.
 // MAx7219 needs to receive the MSB first.
 
-/// Manual bit bang code.
-/// MSB first
+/// Manually send data to the MAX7219 LED matrix.
 pub fn send_data<'a>(
-    data_pin: &'a mut Pin<mode::Output>,
-    clk_pin: &'a mut Pin<mode::Output>,
-    cs_pin: &'a mut Pin<mode::Output>,
+    data_pin: &'a mut Pin<Output, Dynamic>,
+    clk_pin: &'a mut Pin<Output, Dynamic>,
+    cs_pin: &'a mut Pin<Output, Dynamic>,
     data: u8,
     address: u8,
 ) {
     // Combine the address to the first 8 bits and then append the data in the last 8 bits.
     let mut serialized = (address as u16) << 8 | (data as u16);
-    // prepare to receive data.
+    cs_pin.set_high();
+    arduino_hal::delay_ns(50);
     cs_pin.set_low();
+    // prepare to receive data.
     clk_pin.set_low();
+    arduino_hal::delay_ns(50);
 
     // Serially shift the 16 data bits into shift register (address + data).
     for _ in 0..16 {
@@ -87,4 +91,25 @@ pub fn send_data<'a>(
     }
     // pulse to load to register address.
     cs_pin.set_high();
+    arduino_hal::delay_ns(50);
+    cs_pin.set_low();
+}
+
+pub fn set_matrix<'a>(
+    data_pin: &'a mut Pin<Output, Dynamic>,
+    clk_pin: &'a mut Pin<Output, Dynamic>,
+    cs_pin: &'a mut Pin<Output, Dynamic>,
+    data: [u8; 8]
+) {
+    // Initialize the LED matrix
+    // TODO: This needs to be ported into a matrix struct to only initialize once!
+
+    // ...
+    
+    // Digit 0 = 0x1, Digit 7 - 0x8.
+    for i in 1..=8u8 {
+        let address = i;
+        let byte = data[(i as usize) - 1];
+        send_data(data_pin, clk_pin, cs_pin, byte, address);
+    }
 }
